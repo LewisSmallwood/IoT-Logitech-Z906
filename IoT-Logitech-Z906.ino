f654ea1fa03046870ab36fe17e14ebe1630656bf
LewisSmallwood <lewis@smallwood.it>
2023-12-15 20:27:13 +0000

    Implement endpoints

--- IoT-Logitech-Z906.ino
+++ IoT-Logitech-Z906.ino
@@ -5,6 +5,7 @@
 #include <Arduino.h>
 #include "./environment.h"
 #include "./Z906.h"
+#include "./endpoints.h"
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 #include <ESP8266WebServer.h>
@@ -21,97 +22,138 @@ Z906 LOGI(Serial1);
  * Setup the serial communication with the Z906.
  */
 void initSerial() {
-  Serial.begin(9600);
-  while(!Serial);
+    Serial.begin(9600);
+    while(!Serial);
 }
 
 /**
  * Setup and connect to a WiFi network.
  */
 void initWiFi() {
-  // Setup station mode.
-  WiFi.mode(WIFI_STA);
-
-  // Stored WiFi credentials.
-  for (Network network : network_credentials) WiFiMulti.addAP(network.ssid, network.password);
-  delay(2000);
-
-  // Connect to the WiFi.
-  connectToWiFi("Connecting");
+    // Setup station mode.
+    WiFi.mode(WIFI_STA);
+  
+    // Stored WiFi credentials.
+    for (Network network : network_credentials) WiFiMulti.addAP(network.ssid, network.password);
+    delay(2000);
+  
+    // Connect to the WiFi.
+    connectToWiFi("Connecting");
 }
 
 /**
  * Connect to the WiFi.
  */
 void connectToWiFi(const String& message) {
-  timer = 1;
-  Serial.printf("\n[SETUP] %s (%d second)...\n", message.c_str(), timer);
-  
-  while (WiFiMulti.run() != WL_CONNECTED) {
-    timer++;
-    Serial.printf("[SETUP] %s (%d seconds)...\n", message.c_str(), timer);
-    delay(1000);
-    Serial.flush();
-  }
-  
-  onConnected();
+    timer = 1;
+    Serial.printf("\n[SETUP] %s (%d second)...\n", message.c_str(), timer);
+    
+    while (WiFiMulti.run() != WL_CONNECTED) {
+      timer++;
+      Serial.printf("[SETUP] %s (%d seconds)...\n", message.c_str(), timer);
+      delay(1000);
+      Serial.flush();
+    }
+    
+    onConnected();
 }
 
 /**
  * When connection is restored, log the details.
  */
 void onConnected() {
-  timer = 0;
-  Serial.flush();
-  Serial.print("[SETUP] Connected to ");
-  Serial.print(WiFi.SSID());
-  Serial.print(" (");
-  Serial.print(WiFi.localIP());
-  Serial.println(").");
+    timer = 0;
+    Serial.flush();
+    Serial.print("[SETUP] Connected to ");
+    Serial.print(WiFi.SSID());
+    Serial.print(" (");
+    Serial.print(WiFi.localIP());
+    Serial.println(").");
+    
+    // Set the hostname,
+    WiFi.hostname("LOGITECH-Z906");
+    
+    // Configure MDNS.
+    if (MDNS.begin("logitech-z906")) {
+        Serial.println("[SETUP] MDNS responder started.");
+    }
   
-  // Set the hostname,
-  WiFi.hostname("LOGITECH-Z906");
-  
-  // Configure MDNS.
-  if (MDNS.begin("logitech-z906")) {
-    Serial.println("[SETUP] MDNS responder started.");
-  }
-
-  Serial.println();
+    Serial.println();
 }
 
 /**
  * Setup the web server.
  */
 void initWebServer() {
-  server.onNotFound(handleRequest);
-  server.begin();
+    server.onNotFound(handleRequest);
+    server.begin();
 }
 
 /**
  * Handle a HTTP request.
  */
 void handleRequest() {
-  String endpoint = server.uri();;
-    
-  if (endpoint == "/") return server.send(200, "text/plain", "This is the Logitech Z906 API.");
-  if (endpoint.length() > 1) endpoint = endpoint.substring(1);
+    String endpoint = server.uri();;
+      
+    if (endpoint == "/") return server.send(200, "text/plain", "This is the Logitech Z906 API.");
+    if (endpoint.length() > 1) endpoint = endpoint.substring(1);
+  
+    // Check if the Z906 is connected before processing any commands.
+    if (LOGI.request(VERSION) == 0) return server.send(200, "application/json", "{\"status\": \"disconnected\"}");
+  
+    // Handle defined endpoints.
+    for (Endpoint e : endpoints) {
+        if (e.path == endpoint) return respondToRequest(e);
+    }
+  
+    // If not found, redirect to home.
+    server.sendHeader("Location", String("/"), true);
+    server.send(302, "text/plain", "");
+}
 
-  // Check if the Z906 is connected before processing any commands.
-  if (LOGI.request(VERSION) == 0) return server.send(200, "application/json", "{\"status\": \"disconnected\"}");
+/**
+ * Respond to a HTTP request for the given endpoint.
+ */
+void respondToRequest(Endpoint endpoint) {
+    if (endpoint.type == SelectInput) {
+        LOGI.input(endpoint.action);
+        return server.send(200, "application/json", "{\"status\": \"connected\", \"success\": true }");
+        
+    } else if (endpoint.type == RunCommand) {
+        LOGI.cmd(endpoint.action);
+        return server.send(200, "application/json", "{\"status\": \"connected\", \"success\": true }");
+        
+    } else if (endpoint.type == SetValue) {
+        // TODO: Get and validate the inputted value - fail if invaild.
+        LOGI.cmd(endpoint.action, 15);
+        return server.send(501, "application/json", "{\"status\": \"connected\", \"success\": false, \"message\": \"Under construction.\" }");
+        
+    } else if (endpoint.type == GetValue) {
+        return server.send(200, "application/json", "{\"status\": \"connected\", \"success\": true, \"value\": "+String(LOGI.request(endpoint.action))+" }");
 
-  // Handle temperature monitoring.
-  if (endpoint == "temperature") return server.send(200, "application/json", "{\"status\": \"connected\", \"temperature\": "+String(LOGI.main_sensor())+"}");
+    } else if (endpoint.type == RunFunction) {
+        if (endpoint.path == "temperature") return handleGetTemperature();
+        if (endpoint.path == "power/off") return handlePowerOff();
+    }
 
-  // TODO: Setup other endpoints. e.g:
-  // Power ON amplifier:    LOGI.on();
-  // Select RCA 2.0 Input:  LOGI.input(SELECT_INPUT_2);
-  // Disable Mute:          LOGI.cmd(MUTE_OFF);
-  // Set Main Level to 15:  LOGI.cmd(MAIN_LEVEL, 15);
+    return server.send(405, "application/json", "{\"status\": \"connected\", \"success\": false, \"message\": \"Your action was recognised, but it is not supported.\" }");
+}
 
-  // If not found, redirect to home.
-  server.sendHeader("Location", String("/"), true);
-  server.send(302, "text/plain", "");
+/**
+ * Handle the getTemperature function.
+ */
+void handleGetTemperature() {
+    uint8_t value = LOGI.main_sensor();
+    if (value > 0) return server.send(200, "application/json", "{\"status\": \"connected\", \"success\": true, \"value\": "+String(value)+" }");
+    return server.send(200, "application/json", "{\"status\": \"connected\", \"success\": false }");
+}
+
+/**
+ * Gracefully turn the power off, saving the last used state.
+ */
+void handlePowerOff() {
+    LOGI.off();
+    return server.send(200, "application/json", "{\"status\": \"connected\", \"success\": true }");
 }
 
 /**
---
+++ endpoints.h
@@ -0,0 +1,63 @@
+enum EndpointType { SelectInput, RunCommand, SetValue, GetValue, RunFunction };
+
+struct Endpoint {
+  const String path;
+  const EndpointType type;
+  const uint8_t action;
+};
+
+const Endpoint endpoints[] = {  
+  { "input/1", SelectInput, SELECT_INPUT_1 },               // Swap to the TRS 5.1 input
+  { "input/2", SelectInput, SELECT_INPUT_2 },               // Swap to the RCA 2.0 input
+  { "input/3", SelectInput, SELECT_INPUT_3 },               // Swap to the Optical 1 input
+  { "input/4", SelectInput, SELECT_INPUT_4 },               // Swap to the Optical 2 input
+  { "input/5", SelectInput, SELECT_INPUT_5 },               // Swap to the Coaxial input
+
+  { "input/enable", RunCommand, NO_BLOCK_INPUTS },          // Enable signal input
+  { "input/disable", RunCommand, BLOCK_INPUTS },            // Disable signal input
+  
+  { "volume/main", GetValue, MAIN_LEVEL },                  // Get the current Main Level
+  { "volume/main/set", SetValue, MAIN_LEVEL },              // Set the Main Level to the parameter value
+  { "volume/main/up", RunCommand, LEVEL_MAIN_UP },          // Increase Main Level by one unit
+  { "volume/main/down", RunCommand, LEVEL_MAIN_DOWN },      // Decrease Main Level by one unit
+
+  { "volume/subwoofer", GetValue, SUB_LEVEL },              // Get the current Subwoofer Level
+  { "volume/subwoofer/set", SetValue, SUB_LEVEL },          // Set the Subwoofer Level to the parameter value
+  { "volume/subwoofer/up", RunCommand, LEVEL_SUB_UP },      // Increase Subwoofer Level by one unit
+  { "volume/subwoofer/down", RunCommand, LEVEL_SUB_DOWN },  // Decrease Subwoofer Level by one unit
+
+  { "volume/center", GetValue, CENTER_LEVEL },              // Get the current Center Level
+  { "volume/center/set", SetValue, CENTER_LEVEL },          // Set the Center Level to the parameter value
+  { "volume/center/up", RunCommand, LEVEL_CENTER_UP },      // Increase Center Level by one unit
+  { "volume/center/down", RunCommand, LEVEL_CENTER_DOWN },  // Decrease Center Level by one unit
+
+  { "volume/main", GetValue, MAIN_LEVEL },                  // Get the current Main Level
+  { "volume/main/set", SetValue, MAIN_LEVEL },              // Set the Main Level to the parameter value
+  { "volume/main/up", RunCommand, LEVEL_MAIN_UP },          // Increase Main Level by one unit
+  { "volume/main/down", RunCommand, LEVEL_MAIN_DOWN },      // Decrease Main Level by one unit
+
+  { "volume/rear", GetValue, REAR_LEVEL },                  // Get the current Rear Level
+  { "volume/rear/set", SetValue, REAR_LEVEL },              // Set the Rear Level to the parameter value
+  { "volume/rear/up", RunCommand, LEVEL_REAR_UP },          // Increase Rear Level by one unit
+  { "volume/rear/down", RunCommand, LEVEL_REAR_DOWN },      // Decrease Rear Level by one unit
+
+  { "input/decode/on", RunCommand, SELECT_EFFECT_51 },      // Enable Decode Mode
+  { "input/decode/off", RunCommand, DISABLE_EFFECT_51 },    // Disable Decode Mode
+
+  { "input/effect/3d", RunCommand, SELECT_EFFECT_3D },      // Enable 3D Effect on the current input
+  { "input/effect/4.1", RunCommand, SELECT_EFFECT_41 },     // Enable 4.1 Effect on the current input
+  { "input/effect/2.1", RunCommand, SELECT_EFFECT_21 },     // Enable 2.1 Effect on the current input
+  { "input/effect/off", RunCommand, SELECT_EFFECT_NO },     // Disable all Effects on the current input
+
+  { "save", RunCommand, EEPROM_SAVE },                      // Save current settings to EEPROM*
+  { "mute/on", RunCommand, MUTE_ON },                       // Enable mute
+  { "mute/off", RunCommand, MUTE_OFF },                     // Disable mute
+  { "temperature", RunFunction, 0 },                        // Get the system temperature
+  { "version", GetValue, VERSION },                         // Get the system firmware version
+  { "power", GetValue, GET_STATUS },                        // Get the system standby status
+  { "power/on", RunCommand, PWM_OFF },                      // Turn the system on
+  { "power/off", RunFunction, 0 },                          // Turn the system off
+};
+
+// CURRENT_INPUT
+// GET_PWR_UP_TIME
